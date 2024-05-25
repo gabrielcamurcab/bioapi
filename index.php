3b8af000892562349c501da0a0e57724b5f00c0d
Gabriel Camurça <projetosdocamurca@gmail.com>
2024-05-25 16:09:48 -0300

    404 routes

--- index.php
+++ index.php
@@ -8,6 +8,8 @@ use App\Controllers\HeaderDataController;
 use App\Controllers\SocialNetworksController;
 use App\Controllers\MinibioController;
 use App\Controllers\EventsController;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
 
 $app = AppFactory::create();
 
@@ -17,5 +19,15 @@ $app->get('/header', HeaderDataController::class . ':index');
 $app->get('/social', SocialNetworksController::class . ':index');
 $app->get('/minibio', MinibioController::class . ':index');
 $app->get('/event', EventsController::class . ':index');
+$app->get('/', function ($request, $response, $args) {
+    $response->getBody()->write(json_encode(['message' => 'API funcionando']));
+    return $response->withHeader('Content-Type', 'application/json');
+});
+
+// Rota para lidar com rotas não encontradas
+$app->map(['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], '/{routes:.+}', function (Request $request, Response $response) {
+    $response->getBody()->write(json_encode(['message' => 'Rota não encontrada', 'code' => 404]));
+    return $response->withHeader('Content-Type', 'application/json')->withStatus(404);
+});
 
 $app->run();
